46a990b237b627018df1d32cfc1933f1e92103ce
Yu Ji <sophia.jiyu@gmai.com>
2023-09-26 17:03:28 +1000

    update task 2 for amenity names

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,23 @@
 $(function () {
-  const amenityIdList = []
+  const amenityDict = {};
+  const amenityList = [];
   $(".amenities input").change(function () {
-    const amenityId = $(this).data("id")
+    const amenityId = $(this).attr("data-id");
+    const amenityName = $(this).attr("data-name");
     if (this.checked) {
-      if (!amenityIdList.includes(amenityId)) {
+      if (!amenityList.includes(amenityId)) {
         //store the Aminity ID in a variable
-        amenityIdList.push(amenityId);
+        amenityDict[amenityId] = amenityName;
+        amenityList.push(amenityName);
       }
     } else {
       //remove the Aminity ID from the varialbe
-      if (amenityIdList.includes(amenityId)) {
-        amenityIdList.splice(amenityIdList.indexOf(amenityId),1);
+      if (amenityList.includes(amenityName)) {
+        amenityList.splice(amenityList.indexOf(amenityName),1);
       }
     }
     //update h4 tag with the list of amenities checked
-    const checkedAmenity = $(this).data("name")
-    $(".amenities h4").text(checkedAmenity);
+    const amenityText = amenityList.join(', ');
+    $(".amenities h4").text(amenityText);
   });
 });
