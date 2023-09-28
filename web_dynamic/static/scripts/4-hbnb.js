b40dfd26f9d098e892c5af4111da0267766cb809
BryceKnight16 <6373@holbertonstudents.com>
2023-09-28 11:38:49 +1000

    updating code to work

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -63,36 +63,37 @@ $(function () {
       });
     },
     })
+  $('button').click(function () {
     $.ajax({
-      url: 'http://localhost:5001/api/v1/places_search/',
-      method: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({ amenities: Object.values(amenityDict) }),
-      dataType: 'json',
-      success: function (data) {
-          const placesSection = $('.places');
-          placesSection.empty();
-          // data is an array of place objects
-          data.forEach(place => {
-              const placeStr = `
-              <article>
-                  <div class="title_box">
-                      <h2>${place.name}</h2>
-                      <div class="price_by_night">$${place.price_by_night}</div>
-                  </div>
-                  <div class="information">
-                      <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
-                      <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
-                      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
-                  </div>
-                  <div class="description">
-                      ${place.description}
-                  </div>
-              </article>
-              `;
-              $('.places').append(placeStr);
-          });
-      }
+        url: 'http://localhost:5001/api/v1/places_search/',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ amenities: Object.values(amenityDict) }),
+        dataType: 'json',
+        success: function (data) {
+            const placesSection = $('.places');
+            placesSection.empty();
+            // data is an array of place objects
+            data.forEach(place => {
+                const placeStr = `
+                <article>
+                    <div class="title_box">
+                        <h2>${place.name}</h2>
+                        <div class="price_by_night">$${place.price_by_night}</div>
+                    </div>
+                    <div class="information">
+                        <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                        <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                        <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+                    </div>
+                    <div class="description">
+                        ${place.description}
+                    </div>
+                </article>
+                `;
+                $('.places').append(placeStr);
+            });
+          }
+    });
   });
 })
-
